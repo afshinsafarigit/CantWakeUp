903c31e2e780584059819c8b2af89532cfc3205b
Woeldhin <32248067+Woeldhin@users.noreply.github.com>
2019-04-06 20:57:57 +0300

    Laser puzzle kinda done
    Needs some love, but kinda works

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -5,5 +5,4 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     public Vector3 hitPoint;
-
 }
--- Assets/Scripts/LaserEmitter.cs
+++ Assets/Scripts/LaserEmitter.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class LaserEmitter : MonoBehaviour
 {
-
+    public bool enabledFromStart;
     public GameObject laserPrefab;
     private GameObject laser;
     private Transform laserTransform;
+    private Laser laserLaser;
     private bool turnedOn;
+    private GameObject target;
 
     // Start is called before the first frame update
     void Start()
     {
         laser = Instantiate(laserPrefab);
         laserTransform = laser.transform;
+        laserLaser = laser.GetComponent<Laser>();
         turnedOn = false;
         laser.SetActive(false);
+        gameObject.GetComponent<Renderer>().enabled = enabledFromStart;
     }
 
-    void LateUpdate()
+    void Update()
     {
         RaycastHit hit;
 
@@ -28,7 +32,40 @@ public class LaserEmitter : MonoBehaviour
             laserTransform.position = Vector3.Lerp(transform.position, hit.point, .5f);
             laserTransform.LookAt(hit.point);
             laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y, hit.distance);
-            laser.GetComponent<Laser>().hitPoint = hit.point;
+            laserLaser.hitPoint = hit.point;
+
+            if (turnedOn)
+            {
+                if (hit.transform.name.Contains("LaserEmitter"))
+                {
+                    if (!target)
+                    {
+                        hit.transform.tag = "Interactable";
+                        hit.transform.GetComponent<Renderer>().enabled = true;
+                        target = hit.transform.gameObject;
+                    }
+                }
+                else if (hit.transform.name.Contains("Body"))
+                {
+                    Debug.Log("You win");
+                }
+                else if (target)
+                {
+                    hit.transform.tag = "";
+                    target.GetComponent<Renderer>().enabled = false;
+                    target = null;
+                }
+            }
+            else if (target)
+            {
+                target.GetComponent<Renderer>().enabled = false;
+                target = null;
+            }
+        }
+
+        if (!gameObject.GetComponent<Renderer>().enabled && turnedOn)
+        {
+            Interact();
         }
     }
 
